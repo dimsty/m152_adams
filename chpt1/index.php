e092f30077839091300081afa5548daaf942f482
dimsty <james.admsr@eduge.ch>
2022-02-03 12:52:57 +0100

    changes

--- chpt1/index.php
+++ chpt1/index.php
@@ -34,15 +34,7 @@ include "view/header.php";
               </div>
           </div>
        
-          <div class="well"> 
-               <form class="form-horizontal" role="form">
-                <h4>What's New</h4>
-                 <div class="form-group" style="padding:14px;">
-                  <textarea class="form-control" placeholder="Update your status"></textarea>
-                </div>
-                <button class="btn btn-primary pull-right" type="button">Post</button><ul class="list-inline"><li><a href=""><i class="glyphicon glyphicon-upload"></i></a></li><li><a href=""><i class="glyphicon glyphicon-camera"></i></a></li><li><a href=""><i class="glyphicon glyphicon-map-marker"></i></a></li></ul>
-              </form>
-          </div>
+         
        
           <div class="panel panel-default">
              <div class="panel-heading"><a href="#" class="pull-right">View all</a> <h4>More Templates</h4></div>
